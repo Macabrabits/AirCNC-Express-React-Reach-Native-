064cabf826c02af73c1c13f54966156f24afe4dd
Victor Mattar <suporte@trustit.com.br>
2019-10-03 22:48:16 -0300

    Files, Folders and DB Setup

--- src/server.js
+++ src/server.js
@@ -1,8 +1,17 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const routes = require('./routes')
 
 const app = express()
+mongoose.connect('mongodb+srv://aircnc:DMAtGFR8Z0CM1GLF@aircnc-5iwgd.mongodb.net/admin?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}) //The second parameter is optional, and serves only to remove a warning
 
-app.get('/', (req, res) => {
-    return res.json({msg: "Hello World"})
-})
+app.use(express.json()) //Setup express for json API
+app.use(routes)
+
+//DMAtGFR8Z0CM1GLF
+
+
 app.listen(3333)
