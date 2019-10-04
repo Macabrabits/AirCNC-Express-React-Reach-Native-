5d1752ca63dd9f52a9fb245400fe7f2728c960dd
Victor Mattar <suporte@trustit.com.br>
2019-10-03 23:52:05 -0300

    MVC Setup, first insert working

--- src/server.js
+++ src/server.js
@@ -3,15 +3,14 @@ const mongoose = require('mongoose')
 const routes = require('./routes')
 
 const app = express()
-mongoose.connect('mongodb+srv://aircnc:DMAtGFR8Z0CM1GLF@aircnc-5iwgd.mongodb.net/admin?retryWrites=true&w=majority', {
+//mongoose.connect('mongodb+srv://aircnc:DMAtGFR8Z0CM1GLF@aircnc-5iwgd.mongodb.net/admin?retryWrites=true&w=majority', {
+mongoose.connect('mongodb://aircnc:DMAtGFR8Z0CM1GLF@aircnc-shard-00-00-5iwgd.mongodb.net:27017,aircnc-shard-00-01-5iwgd.mongodb.net:27017,aircnc-shard-00-02-5iwgd.mongodb.net:27017/aircnc?ssl=true&replicaSet=AirCnC-shard-0&authSource=admin&retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }) //The second parameter is optional, and serves only to remove a warning
 
 app.use(express.json()) //Setup express for json API
 app.use(routes)
-
-//DMAtGFR8Z0CM1GLF
 
 
 app.listen(3333)
