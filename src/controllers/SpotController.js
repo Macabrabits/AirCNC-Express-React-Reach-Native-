fedc3492b504e4b4454e36401b731998b38e2a57
Victor Mattar <suporte@trustit.com.br>
2019-10-05 02:17:12 -0300

    Spot model

---
+++ src/controllers/SpotController.js
@@ -0,0 +1,20 @@
+const Spot = require('../models/Spot')
+module.exports = {
+    index(){
+
+    },
+    show(){
+        
+    },
+    async store(req, res){
+    
+        return res.json({teste: "fafa"})
+    },
+    update(){
+
+    },
+    destroy(){
+
+    },
+}
+
